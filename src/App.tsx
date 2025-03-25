9e6670ad5fb557d1cda1dd88a65a0c88e393aa9d
Trevor <valentinosolischang@gmail.com>
2025-03-24 21:26:47 -0500

    style: enhance loading message and button responsiveness in App component

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <div className="py-7 px-5 flex flex-col gap-8 items-center justify-center relative">
       {loading ? (
-        <p>Loading advice...</p>
+        <p className="font-bold text-quote text-center">Loading advice...</p>
       ) : (
         <div className="flex flex-col text-center gap-4">
-          <span className="text-primary-Neon-Green tracking-widest text-xs font-medium">
+          <span className="text-primary-Neon-Green tracking-widest text-xs font-medium md:text-base">
             ADVICE #{advice?.id}
           </span>
           <p className="md:text-quote">"{advice?.advice}"</p>
@@ -32,7 +32,7 @@ function App() {
 
         <button
           aria-label="Generate a new advice"
-          className="bg-primary-Neon-Green p-3.5 rounded-full w-12 h-12 flex items-center justify-center absolute -bottom-5 hover:shadow-neon transition duration-300 cursor-pointer"
+          className="bg-primary-Neon-Green p-3.5 rounded-full w-12 h-12 flex items-center justify-center absolute -bottom-5 hover:shadow-neon transition duration-300 cursor-pointer md:p-2 md:w-13 md:h-13"
           onClick={handleClick}
         >
           <img src={DiceIcon} alt="" aria-hidden />
