2dc8ce7c592cca41bd2e8ec076dcd90d304e6fd3
Trevor <valentinosolischang@gmail.com>
2025-03-24 21:21:23 -0500

    feat: add loading state to useAdvice hook

--- src/hooks/useAdvice.ts
+++ src/hooks/useAdvice.ts
@@ -7,12 +7,16 @@ type Advice = {
 
 export const useAdvice = () => {
   const [advice, setAdvice] = useState<Advice | null>(null);
+  const [loading, setLoading] = useState(true);
   const API_URL = "https://api.adviceslip.com/advice";
 
   useEffect(() => {
     fetch(API_URL)
       .then((response) => response.json())
-      .then((data) => setAdvice(data.slip))
+      .then((data) => {
+        setAdvice(data.slip);
+        setLoading(false);
+      })
       .catch((error) => console.error("Error fetching advice:", error));
   }, [API_URL]);
 
@@ -23,5 +27,5 @@ export const useAdvice = () => {
       .catch((error) => console.error("Error fetching advice:", error));
   };
 
-  return { advice, handleClick };
+  return { advice, loading, handleClick };
 };
